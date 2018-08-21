316c22af209a99680ce9851a4e7fd3f26bece2f9
Dylan <ddreyer@berkeley.edu>
2018-08-20 21:37:05 -0700

    vendor code additions plus create-react-app

--- server/database.go
+++ server/database.go
@@ -2,14 +2,27 @@ package models
 
 import (
 	"database/sql"
-	"fmt"
 )
 
 func CreateAthleteTables(DB *sql.DB) {
-	fmt.Println(DB)
-	query := `CREATE TABLE maleathletes (lastname VARCHAR(20), firstname VARCHAR(20))`
+	query := `DROP TABLE if exists maleAthletes`
 	if _, err := DB.Exec(query); err != nil {
 		panic(err)
 	}
-
+	query = `CREATE TABLE maleAthletes (lastname VARCHAR(20), firstname VARCHAR(20))`
+	if _, err := DB.Exec(query); err != nil {
+		panic(err)
+	}
+	query = `INSERT into maleAthletes (lastname, firstname) VALUES ('Bridges', 'Josh')`
+	if _, err := DB.Exec(query); err != nil {
+		panic(err)
+	}
+	query = `DROP TABLE if exists femaleAthletes`
+	if _, err := DB.Exec(query); err != nil {
+		panic(err)
+	}
+	query = `CREATE TABLE femaleAthletes (lastname VARCHAR(20), firstname VARCHAR(20))`
+	if _, err := DB.Exec(query); err != nil {
+		panic(err)
+	}
 }
